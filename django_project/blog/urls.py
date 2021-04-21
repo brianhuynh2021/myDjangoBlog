c21e502de5b434e17a4db8da8a060c98d51c1904
brian huynh <huynh2102@gmail.com>
2021-04-22 01:50:01 +0700

    update more about and home templates

---
+++ django_project/blog/urls.py
@@ -0,0 +1,8 @@
+from django import urls
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about', views.about, name='blog-about'),
+]
---
+++ django_project/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+# Create your views here.
+
+posts = [
+    {
+        'author': 'Brian',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 22, 2021'
+    },
+    {
+        'author': 'Viet_lon',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2021'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'About'})
