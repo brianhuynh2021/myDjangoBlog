9f8ee620d54097821536affaebb72f2eb9bf0bad
brian huynh <huynh2102@gmail.com>
2021-05-08 01:15:32 +0700

    complete load page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model><viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
