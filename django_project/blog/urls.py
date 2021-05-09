8b1f14f6a73f649346a6eba4dcf635aeda0d3e80
brian huynh <huynh2102@gmail.com>
2021-05-10 00:09:48 +0700

    reset password completed

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,8 +1,7 @@
-from django import urls
 from django.urls import path
 from .views import (
-    PostListView, 
-    PostDetailView, 
+    PostListView,
+    PostDetailView,
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
@@ -17,6 +16,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('about', views.about, name='blog-about'),
+    path('about/', views.about, name='blog-about'),
 ]
-
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
