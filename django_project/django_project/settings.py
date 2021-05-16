7acdea84980841b629d52a72eb19cba1accfb5fd
brian huynh <huynh2102@gmail.com>
2021-05-16 19:59:20 +0700

    change host

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'bbcc6+ifb=2n#=jmq)c0*3g$%^=f5(r(kt!g%h139l=t(1pze2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','brianhuynhdjango.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','newdjangoapp.herokuapp.com']
 
 
 # Application definition
